c3422ce34b6f90686c1f1ecd11a916b8640e87bb
bshaffer <betterbrent@google.com>
2016-10-10 22:10:32 +0000

    Autogenerated update for sqladmin version v1beta4 (2016-10-10)

--- src/Google/Service/SQLAdmin/DatabaseInstance.php
+++ src/Google/Service/SQLAdmin/DatabaseInstance.php
@@ -19,6 +19,7 @@ class Google_Service_SQLAdmin_DatabaseInstance extends Google_Collection
 {
   protected $collection_key = 'suspensionReason';
   public $backendType;
+  public $connectionName;
   public $currentDiskSize;
   public $databaseVersion;
   public $etag;
@@ -55,6 +56,14 @@ class Google_Service_SQLAdmin_DatabaseInstance extends Google_Collection
   public function getBackendType()
   {
     return $this->backendType;
+  }
+  public function setConnectionName($connectionName)
+  {
+    $this->connectionName = $connectionName;
+  }
+  public function getConnectionName()
+  {
+    return $this->connectionName;
   }
   public function setCurrentDiskSize($currentDiskSize)
   {
