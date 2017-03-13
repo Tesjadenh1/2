493590ba20d4c4003f204c87a662d9edb9ad1ed3
bshaffer <betterbrent@google.com>
2017-03-13 22:02:33 +0000

    Autogenerated update for speech version v1beta1 (2017-03-13)

--- src/Google/Service/Speech.php
+++ src/Google/Service/Speech.php
@@ -90,10 +90,6 @@ class Google_Service_Speech extends Google_Service
               'path' => 'v1beta1/operations',
               'httpMethod' => 'GET',
               'parameters' => array(
-                'filter' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
                 'name' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -105,6 +101,10 @@ class Google_Service_Speech extends Google_Service
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
+                ),
+                'filter' => array(
+                  'location' => 'query',
+                  'type' => 'string',
                 ),
               ),
             ),
