e3c7e201acb8ade7a155573b56691eeea2e6481a
bshaffer <betterbrent@google.com>
2017-03-13 16:24:03 +0000

    Autogenerated update for adexchangebuyer2 version v2beta1 (2017-03-13)

--- src/Google/Service/AdExchangeBuyerII.php
+++ src/Google/Service/AdExchangeBuyerII.php
@@ -308,10 +308,6 @@ class Google_Service_AdExchangeBuyerII extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
-                'query' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -319,6 +315,10 @@ class Google_Service_AdExchangeBuyerII extends Google_Service
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
+                ),
+                'query' => array(
+                  'location' => 'query',
+                  'type' => 'string',
                 ),
               ),
             ),'stopWatching' => array(
