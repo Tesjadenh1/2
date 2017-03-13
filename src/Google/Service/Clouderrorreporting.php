4f35477645b9fdf44c980e58ea24fff63c805e3b
bshaffer <betterbrent@google.com>
2017-03-13 22:00:51 +0000

    Autogenerated update for clouderrorreporting version v1beta1 (2017-03-13)

--- src/Google/Service/Clouderrorreporting.php
+++ src/Google/Service/Clouderrorreporting.php
@@ -89,15 +89,19 @@ class Google_Service_Clouderrorreporting extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
+                'timeRange.period' => array(
+                  'location' => 'query',
+                  'type' => 'string',
+                ),
                 'groupId' => array(
                   'location' => 'query',
                   'type' => 'string',
                 ),
-                'pageToken' => array(
+                'serviceFilter.service' => array(
                   'location' => 'query',
                   'type' => 'string',
                 ),
-                'serviceFilter.service' => array(
+                'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
                 ),
@@ -110,10 +114,6 @@ class Google_Service_Clouderrorreporting extends Google_Service
                   'type' => 'string',
                 ),
                 'serviceFilter.resourceType' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
-                'timeRange.period' => array(
                   'location' => 'query',
                   'type' => 'string',
                 ),
@@ -147,14 +147,6 @@ class Google_Service_Clouderrorreporting extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
-                'timedCountDuration' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
-                'pageToken' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
                 'timeRange.period' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -176,11 +168,11 @@ class Google_Service_Clouderrorreporting extends Google_Service
                   'location' => 'query',
                   'type' => 'integer',
                 ),
-                'serviceFilter.version' => array(
+                'order' => array(
                   'location' => 'query',
                   'type' => 'string',
                 ),
-                'order' => array(
+                'serviceFilter.version' => array(
                   'location' => 'query',
                   'type' => 'string',
                 ),
@@ -189,6 +181,14 @@ class Google_Service_Clouderrorreporting extends Google_Service
                   'type' => 'string',
                 ),
                 'alignmentTime' => array(
+                  'location' => 'query',
+                  'type' => 'string',
+                ),
+                'timedCountDuration' => array(
+                  'location' => 'query',
+                  'type' => 'string',
+                ),
+                'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
                 ),
