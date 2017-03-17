cbae3b54f4edb0c6021d255dd8c7ff60d9ccec76
bshaffer <betterbrent@google.com>
2017-03-17 01:04:58 +0000

    Autogenerated update for cloudkms version v1 (2017-03-17)

--- src/Google/Service/CloudKMS.php
+++ src/Google/Service/CloudKMS.php
@@ -78,10 +78,6 @@ class Google_Service_CloudKMS extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
-                'filter' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -89,6 +85,10 @@ class Google_Service_CloudKMS extends Google_Service
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
+                ),
+                'filter' => array(
+                  'location' => 'query',
+                  'type' => 'string',
                 ),
               ),
             ),
