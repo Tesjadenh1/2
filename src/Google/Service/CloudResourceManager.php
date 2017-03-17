a7611ef67a71ac0a8b894a3720bc1d7138b3cb54
bshaffer <betterbrent@google.com>
2017-03-17 01:05:01 +0000

    Autogenerated update for cloudresourcemanager version v1 (2017-03-17)

--- src/Google/Service/CloudResourceManager.php
+++ src/Google/Service/CloudResourceManager.php
@@ -225,10 +225,6 @@ class Google_Service_CloudResourceManager extends Google_Service
               'path' => 'v1/projects',
               'httpMethod' => 'GET',
               'parameters' => array(
-                'filter' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -236,6 +232,10 @@ class Google_Service_CloudResourceManager extends Google_Service
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
+                ),
+                'filter' => array(
+                  'location' => 'query',
+                  'type' => 'string',
                 ),
               ),
             ),'setIamPolicy' => array(
