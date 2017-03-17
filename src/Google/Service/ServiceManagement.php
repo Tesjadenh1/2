3652f5cf43b32a8661dd14cc70943131f86e8178
bshaffer <betterbrent@google.com>
2017-03-17 01:06:18 +0000

    Autogenerated update for servicemanagement version v1 (2017-03-17)

--- src/Google/Service/ServiceManagement.php
+++ src/Google/Service/ServiceManagement.php
@@ -88,6 +88,10 @@ class Google_Service_ServiceManagement extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
+                'filter' => array(
+                  'location' => 'query',
+                  'type' => 'string',
+                ),
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -95,10 +99,6 @@ class Google_Service_ServiceManagement extends Google_Service
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
-                ),
-                'filter' => array(
-                  'location' => 'query',
-                  'type' => 'string',
                 ),
               ),
             ),
@@ -191,10 +191,6 @@ class Google_Service_ServiceManagement extends Google_Service
               'path' => 'v1/services',
               'httpMethod' => 'GET',
               'parameters' => array(
-                'pageSize' => array(
-                  'location' => 'query',
-                  'type' => 'integer',
-                ),
                 'producerProjectId' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -206,6 +202,10 @@ class Google_Service_ServiceManagement extends Google_Service
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
+                ),
+                'pageSize' => array(
+                  'location' => 'query',
+                  'type' => 'integer',
                 ),
               ),
             ),'setIamPolicy' => array(
