dc91c39c73e8c99e80e106ee416d3052ecb6f410
Google APIs <googleapis-publisher@google.com>
2017-12-12 00:23:16 +0000

    Autogenerated update (2017-12-12)
    Add:
    - cloudshell:v1
    
    Update:
    - cloudfunctions:v1
    - slides:v1
    - streetviewpublish:v1

--- src/Google/Service/CloudFunctions.php
+++ src/Google/Service/CloudFunctions.php
@@ -200,13 +200,13 @@ class Google_Service_CloudFunctions extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
-                'pageToken' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
+                ),
+                'pageToken' => array(
+                  'location' => 'query',
+                  'type' => 'string',
                 ),
               ),
             ),'patch' => array(
