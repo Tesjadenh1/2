319eb3cfcd702de912b2fe25fbfa63e972c3762c
Google APIs <googleapis-publisher@google.com>
2018-02-27 00:23:40 +0000

    Autogenerated update (2018-02-27)
    Update:
    - people:v1

--- src/Google/Service/PeopleService.php
+++ src/Google/Service/PeopleService.php
@@ -132,6 +132,10 @@ class Google_Service_PeopleService extends Google_Service
               'path' => 'v1/contactGroups',
               'httpMethod' => 'GET',
               'parameters' => array(
+                'pageSize' => array(
+                  'location' => 'query',
+                  'type' => 'integer',
+                ),
                 'syncToken' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -139,10 +143,6 @@ class Google_Service_PeopleService extends Google_Service
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
-                ),
-                'pageSize' => array(
-                  'location' => 'query',
-                  'type' => 'integer',
                 ),
               ),
             ),'update' => array(
@@ -226,10 +226,6 @@ class Google_Service_PeopleService extends Google_Service
               'path' => 'v1/people:batchGet',
               'httpMethod' => 'GET',
               'parameters' => array(
-                'personFields' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
                 'requestMask.includeField' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -238,6 +234,10 @@ class Google_Service_PeopleService extends Google_Service
                   'location' => 'query',
                   'type' => 'string',
                   'repeated' => true,
+                ),
+                'personFields' => array(
+                  'location' => 'query',
+                  'type' => 'string',
                 ),
               ),
             ),'updateContact' => array(
