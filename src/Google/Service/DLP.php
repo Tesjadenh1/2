e98871c966b804bb6920ffe082c92074794f68d4
Google APIs <googleapis-publisher@google.com>
2018-03-29 00:23:53 +0000

    Autogenerated update (2018-03-29)
    Update:
    - androidpublisher:v2
    - cloudbuild:v1
    - dialogflow:v2
    - dlp:v2
    - ml:v1

--- src/Google/Service/DLP.php
+++ src/Google/Service/DLP.php
@@ -381,6 +381,10 @@ class Google_Service_DLP extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
+                'filter' => array(
+                  'location' => 'query',
+                  'type' => 'string',
+                ),
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -390,10 +394,6 @@ class Google_Service_DLP extends Google_Service
                   'type' => 'integer',
                 ),
                 'type' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
-                'filter' => array(
                   'location' => 'query',
                   'type' => 'string',
                 ),
