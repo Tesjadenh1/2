a258731aa5193991c4f2abae4c1fc465812696c3
Google APIs <googleapis-publisher@google.com>
2018-09-02 00:22:51 +0000

    Autogenerated update (2018-09-02)
    Update:
    - redis:v1beta1

--- src/Google/Service/CloudRedis.php
+++ src/Google/Service/CloudRedis.php
@@ -215,10 +215,6 @@ class Google_Service_CloudRedis extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
-                'filter' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -226,6 +222,10 @@ class Google_Service_CloudRedis extends Google_Service
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
+                ),
+                'filter' => array(
+                  'location' => 'query',
+                  'type' => 'string',
                 ),
               ),
             ),
