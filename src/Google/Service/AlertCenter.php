8c840935dff4ce834089f3e1df5b95be6258ee3b
Google APIs <googleapis-publisher@google.com>
2018-12-11 00:22:52 +0000

    Autogenerated update (2018-12-11)
    Update:
    - alertcenter:v1beta1

--- src/Google/Service/AlertCenter.php
+++ src/Google/Service/AlertCenter.php
@@ -88,6 +88,10 @@ class Google_Service_AlertCenter extends Google_Service
               'path' => 'v1beta1/alerts',
               'httpMethod' => 'GET',
               'parameters' => array(
+                'filter' => array(
+                  'location' => 'query',
+                  'type' => 'string',
+                ),
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -103,10 +107,6 @@ class Google_Service_AlertCenter extends Google_Service
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
-                ),
-                'filter' => array(
-                  'location' => 'query',
-                  'type' => 'string',
                 ),
               ),
             ),'undelete' => array(
@@ -152,11 +152,11 @@ class Google_Service_AlertCenter extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
-                'customerId' => array(
+                'filter' => array(
                   'location' => 'query',
                   'type' => 'string',
                 ),
-                'filter' => array(
+                'customerId' => array(
                   'location' => 'query',
                   'type' => 'string',
                 ),
