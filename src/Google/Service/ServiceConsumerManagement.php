fd9125fcbb9e6aeb47531133978988f47f2fa2b7
Google APIs <googleapis-publisher@google.com>
2019-01-03 00:23:03 +0000

    Autogenerated update (2019-01-03)
    Update:
    - bigquery:v2
    - serviceconsumermanagement:v1
    - serviceusage:v1

--- src/Google/Service/ServiceConsumerManagement.php
+++ src/Google/Service/ServiceConsumerManagement.php
@@ -211,15 +211,15 @@ class Google_Service_ServiceConsumerManagement extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
+                'pageToken' => array(
+                  'location' => 'query',
+                  'type' => 'string',
+                ),
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
                 ),
                 'filter' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
-                'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
                 ),
